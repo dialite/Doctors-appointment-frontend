a0ffb4ae5e41057e322dbf110e901e70e30aad8f
Aduda-Boaz <adudaboaz@gmail.com>
2023-04-07 22:32:13 +0300

    Add single doctor component

--- src/components/doctors/SingleDoctor.jsx
+++ src/components/doctors/SingleDoctor.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import {
   FaTwitter, FaFacebookF, FaInstagramSquare, FaLinkedin,
 } from 'react-icons/fa';
-import { getDoctor } from '../../redux/doctors/doctors';
+import { getDoctorsAction } from '../../redux/doctors/doctors';
+import baseUrl from '../helpers/data';
 
 const socialIcons = [
   { icon: FaFacebookF },
@@ -12,10 +13,40 @@ const socialIcons = [
   { icon: FaLinkedin },
 ];
 
-const SingleDoctor = () => (
+const SingleDoctor = ({
+  name, lastname, image, speciality, experience,
+}) => (
   <div className="doctor1">
-    
+    <div className="doctor1__img">
+      <img src={`${baseUrl}${image}`} alt="doctor" />
+    </div>
+    <div className="doctor1__info">
+      <h3 className="doctor1__name">
+        {name}
+        {' '}
+        {lastname}
+      </h3>
+      <p>................................</p>
+      <p>{speciality}</p>
+      <p>
+        {experience}
+        years of experience
+      </p>
+    </div>
+    <div className="doctor1__social">
+      {socialIcons.map((social) => (
+        <social.icon key={social.icon} />
+      ))}
+    </div>
   </div>
 );
+
+SingleDoctor.propTypes = {
+  name: PropTypes.string.isRequired,
+  lastname: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  speciality: PropTypes.string.isRequired,
+  experience: PropTypes.number.isRequired,
+};
 
 export default SingleDoctor;
---
+++ src/components/helpers/data.js
@@ -0,0 +1,3 @@
+const baseUrl = 'https://json-api-nro9.onrender.com/doctors';
+
+export default baseUrl;
