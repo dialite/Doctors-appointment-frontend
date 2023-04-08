6c39e37b861924f3ea5556daf9cfcb4e65a5a2c8
Aduda-Boaz <adudaboaz@gmail.com>
2023-04-08 20:40:55 +0300

    Modify the home page to fix erros

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
+/* eslint-disable react/jsx-key */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-multi-carousel';
 import SingleDoctor from '../components/doctors/SingleDoctor';
-import { getDoctorsAction } from '../../redux/doctors/doctors';
+import 'react-multi-carousel/lib/styles.css';
+import { getDoctorsAction } from '../redux/doctors/doctors';
 
-const Home = {
-  doctors = useSelector((state) => state.doctors),
-  dispatch = useDispatch(),
+const Home = () => {
+  const doctors = useSelector((state) => state.doctors);
+  const dispatch = useDispatch();
 
-  useEffect (() => {
+  useEffect(() => {
     dispatch(getDoctorsAction());
-  }, []);
+  }, [dispatch]);
 
-  responsive = {
+  const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
@@ -29,36 +32,39 @@ const Home = {
       breakpoint: { max: 464, min: 0 },
       items: 1,
     },
-  },
-
-return (
-  <div className="home-page">
-    {
-      doctors.length > 0 && (
-        <><div className="home-page__hero">
+  };
+  return (
+    <div className="home-page">
+      <>
+        <div className="home-page__hero">
           <h1>DOCTORS ONLINE</h1>
           <p>Find the best doctors in your area</p>
-        </div><div className="home-page__doctors">
-            <Carousel
-            responsive={responsive}
-            className="doctor-carousel">
-              {doctors.map((doctor) => (
-                <Link to={`/doctors_details/${doctor.id}`}>
-                  <SingleDoctor
-                    key={doctor.id}
-                    name={doctor.name}
-                    lastname={doctor.lastname}
-                    image={doctor.image}
-                    speciality={doctor.speciality}
-                    experience={doctor.experience} />
-                </Link>
-              ))}
-            </Carousel>
-          </div></>
         </div>
-      )
-    }
-  </div>    
+        {
+          doctors && (
+            <div className="home-page__doctors">
+              <Carousel
+                responsive={responsive}
+                className="doctor-carousel"
+              >
+                {doctors.map((doctor) => (
+                  <Link to={`/doctors_details/${doctor.id}`}>
+                    <SingleDoctor
+                      name={doctor.name}
+                      lastname={doctor.lastname}
+                      image={doctor.image}
+                      speciality={doctor.speciality}
+                      experience={doctor.experience}
+                    />
+                  </Link>
+                ))}
+              </Carousel>
+            </div>
+          )
+        }
+      </>
+    </div>
+  );
 };
 
 export default Home;
