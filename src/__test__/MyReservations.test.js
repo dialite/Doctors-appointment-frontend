6da583aa30c56ec160015f79eb64d7a84db19b34
Aduda-Boaz <adudaboaz@gmail.com>
2023-04-10 20:08:17 +0300

    Add MyReservations tests

---
+++ src/__test__/MyReservations.test.js
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import MyReservations from '../pages/MyReservations/MyReservations';
+import store from '../redux/configureStore';
+
+describe('MyReservations', () => {
+  it('renders correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <MyReservations />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('has a MyReservations button', () => {
+    const myReservations = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <MyReservations />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(myReservations.findByText('My Reservations')).toMatchSnapshot();
+  });
+});
